03ab1f486deb7b2616b5c237b8140a6641c1e3e0
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-06-14 07:19:18 -0700

    2D wave traversal done

--- Basic/2D Array/index.cpp
+++ Basic/2D Array/index.cpp
@@ -1,36 +1,110 @@
 // Online C++ compiler to run C++ program online
-// Just for practicing
 #include <iostream>
 using namespace std;
 
+bool isPrime(int arr[][4],int search, int row, int col){
+    for (int row=0;row<3;row++){
+        for(int col=0;col<4;col++){
+            if (arr[row][col]==search){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void rowWiseSum(int arr[][4]){
+    int sum=0;
+    for (int row=0;row<3;row++){
+        sum=0;
+        for (int col=0;col<4;col++){
+            sum+=arr[row][col];
+        }
+        cout<<row<<" Row Sum is "<<sum;
+        cout<<endl;
+    }
+    
+}
+
+void colWiseSum(int arr[][4]){
+    int sum=0;
+    for (int row=0;row<4;row++){
+        sum=0;
+        for (int col=0;col<3;col++){
+            sum+=arr[col][row];
+        }
+        cout<<row<<" Col Sum is "<<sum;
+        cout<<endl;
+    }
+    
+}
+
+
+void largestRowSum(int arr[][4]){
+    int max=0;
+    int rowIndex=-1;
+    for (int row=0;row<3;row++){
+        int sum=0;
+        for (int col=0;col<4;col++){
+            sum+=arr[row][col];
+        }
+        if (sum>max){
+            max=sum;
+            rowIndex=row;
+        }
+    }
+    cout<<"Largest Sum row is "<<rowIndex<<endl;
+    cout<<"Largest Sum is "<<max<<endl;
+    
+}
+
+void wavePrint(int arr[][4]){
+     for (int col=0;col<4;col++){
+        //int sum=0;
+        if (col%2==0){
+            
+            for (int row=0;row<3;row++){
+                cout<<arr[row][col];
+            }
+            
+        }
+        else{
+            for (int row=3-1;row>=0;row--){
+                cout<<arr[row][col];
+            }
+        cout<<endl;
+        }
+        
+    }
+}
+
 int main() {
     // Write C++ code here
-    // cout << "Hello world!";
+    // std::cout << "Hello world!";
     int arr[3][4];
-    // int arr[3][4]={1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12};//memory ma ayesy store hota ha but visualize matrix ki tarah krengy
-    // formula for putting memory elements into matrix is col*i+j where col= total cols, i=row index, j= col index
-    
-    // int arr[3][4]={{1 ,2 ,3 ,4} ,{5 ,6 ,7 ,8} ,{9 ,10 ,11 ,12}}; is tarah bhi krskty han
-    //taking column wise input
-    // for (int row=0;row<4;row++){
-    //     for (int col=0;col<3;col++){
-    //         cin>>arr[col][row];
-    //     }
-    // }
-    
-    // taking row wise input
-    for (int row=0;row<3;row++){
+    for (int row=0; row<3;row++){
         for (int col=0;col<4;col++){
             cin>>arr[row][col];
         }
     }
-    
-    // cout<<arr
-    for (int row=0;row<3;row++){
+    for (int row=0; row<3;row++){
         for (int col=0;col<4;col++){
             cout<<arr[row][col]<<" ";
         }
         cout<<endl;
     }
+    int search;
+    cin>>search;
+    if (isPrime(arr,search,3,4)){
+        cout<<"Found"<<endl;
+    }
+    else{
+        cout<<"Not Found"<<endl;
+    }
+    rowWiseSum(arr);
+    colWiseSum(arr);
+    largestRowSum(arr);
+    wavePrint(arr);
+    // cout<<arr;
     return 0;
 }
