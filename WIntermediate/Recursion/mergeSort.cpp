1f799c476e962cca92f15a69acd72d2b1c18d3fd
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-08-26 09:32:22 -0700

    merge and quick done

---
+++ WIntermediate/Recursion/mergeSort.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+
+void mergeArr(int * arr,int s , int e){
+    int mid=s+(e-s)/2;
+
+    int len1=mid-s+1;
+    int len2=e-mid;
+
+    int *first=new int[len1];
+    int *second=new int[len2];
+
+    int mainArrayIndex=s;
+
+    for (int i=0; i<len1; i++){
+        first[i]=array[mainArrayIndex++];
+    }
+
+    for (int i=0; i<len2; i++){
+        second[i]=array[mainArrayIndex++];
+    }
+
+    int index1=0;
+    int index2=0;  
+    mainArrayIndex=s;
+
+    while(index1<len1 && index2<len2){
+        if(first[index1]<second[index2] ){
+            arr[mainArrayIndex++]=first[index1++];
+        }
+        else {
+            arr[mainArrayIndex++]=second[index2++];
+        }
+    }
+
+    while(index1<len1){
+     arr[mainArrayIndex++]=first[index1++];   
+    }
+
+    while(index2<len2){
+     arr[mainArrayIndex++]=second[index2++];   
+    }
+
+    delete[] first;
+    delete[] second;
+    
+}
+
+
+void mergeSort(int *arr,int s, int e){
+    if (s>=e){
+        return;
+    }
+
+    int mid=s+(e-s)/2;
+
+    //left part sort krna ha
+    mergeSort(arr,s,mid);
+
+    //right part sort krna ha
+    mergeSort(arr,mid+1,e);
+
+    mergeArr(arr,s,e);
+}
+
+int main(){
+
+    int arr[5]={3,5,8,4,2};
+
+    mergeSort(arr,0,4);
+
+    for (int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
---
+++ WIntermediate/Recursion/quickSort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[],int s,int e){
+    int pivot=arr[s];
+    int cnt=0;
+
+
+    for (int i=s+1;i<=e;i++){
+        if (pivot>=arr[i]){
+            cnt++;
+        }
+    }
+    
+    int pivotIndex=s+cnt;
+    swap(arr[pivotIndex],arr[s]);
+    
+    int i=s; 
+    int j=e;
+
+    while(i<pivotIndex && j>pivotIndex){
+        while(arr[i]<=pivotIndex){
+            i++;
+        }
+        while(arr[j]>pivotIndex){
+            j--;        
+        }
+
+        if(i<pivotIndex && j>pivotIndex){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+    
+}
+
+
+void quickSort(int arr[],int s,int e){
+    if(s>=e){
+        return;
+    }
+
+    int p=partition(arr,s,e);
+
+    // left part ko sort kro
+    quickSort(arr,s,p-1);
+
+    //right part ko sort kro
+    quickSort(arr,p+1,e);
+
+
+}
+
+int main(){
+    
+    int arr[10]={0,31,23,3,14,45,6,5,2,8};
+    int n=10;
+
+    quickSort(arr,0,n-1);
+
+
+    return 0;
+}
